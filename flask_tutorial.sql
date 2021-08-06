95aef86aef1ac782a3b86c01ffa1b659bfe6b9fc
Alok <aalokvsingh@gmail.com>
2021-08-06 00:55:21 -0700

    added post module

--- flask_tutorial.sql
+++ flask_tutorial.sql
@@ -9,3 +9,14 @@ CREATE TABLE user (
     status int(1) DEFAULT 1,
     PRIMARY KEY (id)
 );
+
+DROP TABLE IF EXISTS user;
+CREATE TABLE post (
+    id int NOT NULL AUTO_INCREMENT,
+    ptitle varchar(255) NOT NULL,
+    pcontent varchar(255),
+    create_at varchar(255) NOT NULL,
+    image_path varchar(255) NOT NULL,
+    pstatus int(1) DEFAULT 1,
+    PRIMARY KEY (id)
+);
